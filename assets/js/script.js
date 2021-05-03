7a4eae626a13980364d96dee863ede111a8e5807
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-05-03 15:34:54 -0700

    added modal  responses and search by enter

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,37 @@ $(document).foundation();
 
 $("#search-button").on("click", function(event){
     event.preventDefault();
-    // $('exampleModal2').foundation('reveal', 'open');
     let searchInput =  $("#search-input").val();
     let apiLink = `https://gateway.marvel.com/v1/public/characters?name=${ searchInput }&ts=1&apikey=2ef8d58d6e4c1a6eb9fe640436563e2c&hash=4b46213c75452f9fc065e74ea4d8d2d3`
     fetch(apiLink).then(function(response){
         if (response.status == 200) {
             response.json().then(function (data) {
-                console.log("CharacterData: " , data);
-                displayInfo(data)
-                let giphyApi = `https://api.giphy.com/v1/gifs/search?api_key=nwuYq2Fo9Ze7lf358CgrL7CJzWpVdYMG&q=${ searchInput }&limit=80&offset=0&rating=g&lang=en`
-                fetch(giphyApi).then(function(giphyData){
-                    if (giphyData.status == 200){
-                        giphyData.json().then(function (giphyData) {
-                        console.log("giphyData: " , giphyData);
-                        displayGiphys(giphyData);
+                if(data.data.count !== 0){
+                    console.log("CharacterData: " , data);
+                    displayInfo(data)
+                    let giphyApi = `https://api.giphy.com/v1/gifs/search?api_key=nwuYq2Fo9Ze7lf358CgrL7CJzWpVdYMG&q=${ searchInput }&limit=80&offset=0&rating=g&lang=en`
+                    fetch(giphyApi).then(function(giphyData){
+                        if (giphyData.status == 200){
+                            giphyData.json().then(function (giphyData) {
+                            console.log("giphyData: " , giphyData);
+                            displayGiphys(giphyData);
                     })
-                    } else {
-                        console.log("Bad Request!")
+                        } else {
+                            $("#search-input").val("");
+                            $('#Nothing-Found').foundation("open");
                     }
                 })
+                } else {
+                    $("#search-input").val("");
+                    $('#Nothing-Found').foundation("open");
+                }
             })
         } else {
-            alert("Please enter valid character name")
+            $('#modalBadRequest').foundation("open");
         }
     })
     .catch(function(){
-        console.log("Bad Request!")
+        $('#modalBadRequest').foundation("open");
     })
 })
 
@@ -79,9 +84,15 @@ function displayInfo(rawData){
         // console.log(rawData)
             } 
  else {
-    alert("Nothing Found")
+    $('#modalWrongCharacterName').foundation("open");
 }
 }
+//--------- search when we hit enter
+$('#search-input').keypress(function(e){
+    console.log(e)
+    if(e.keyCode == 13)
+    $('#search-button').click();
+  });
 
 function displayGiphys(giphyData){
     let y=0;
