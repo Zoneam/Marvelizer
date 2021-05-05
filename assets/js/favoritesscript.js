4097f0848ae56f771595499ed35a283be2fc2f97
Matt Linder <mjlinder218@gmail.com>
2021-05-04 17:37:39 -0700

    Added function to display list of searched names

---
+++ assets/js/favoritesscript.js
@@ -0,0 +1,33 @@
+var names = [];
+
+function displaySearchNames() {
+  var existingEntries = JSON.parse(localStorage.getItem("marvelizer"));
+  if (existingEntries !== null) {
+    for (let i = 0; i < existingEntries.length; i++) {
+      var storedName = existingEntries[i].name;
+      // If name not in array
+      if (!names.includes(storedName)) {
+        names.push(storedName);
+      }
+    }
+    for (let i =  0; i < names.length; i++) {
+      let searchLi = $("<li>")
+        .addClass("search-name")
+        .appendTo(".search-list");
+        searchLi.text(names[i]); 
+    }
+  }
+}
+
+displaySearchNames()
+
+// event listener for list item clicks
+// let listClickEl = $(".search-name");  
+// listClickEl.on("click", function(event){
+//   event.preventDefault();
+//   console.log(event.target.text);
+//   let clickedItem = event.target;
+//   console.log(clickedItem + "  ---------");
+//   clickedItemText = clickedItem.text
+//   console.log(clickedItemText + "  ++++++++");
+// })
