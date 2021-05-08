52985965ed4ff2b16af804591c388f642b287b86
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-05-07 20:58:28 -0700

    Beau Barrier commit

--- assets/js/favoritesscript.js
+++ assets/js/favoritesscript.js
@@ -4,14 +4,14 @@ var namePositionArray = []
 
 function displaySearchNames() {
   if (existingEntries !== null) {
-    for (let i =  0; i < existingEntries.characterNames.length; i++) {
+    for (let i = 0; i < existingEntries.characterNames.length; i++) {
       let searchLi = $("<li>")
         .addClass("search-name")
         .attr("id", i)
         .appendTo(".search-list");
-      searchLi.text(existingEntries.characterNames[i]); 
+      searchLi.text(existingEntries.characterNames[i]);
       namePositionArray.push(existingEntries.characterNames[i]);
-    } 
+    }
   } else {
     let noGifMessage = $("<p>")
       .addClass("no-gif-message")
@@ -21,65 +21,54 @@ function displaySearchNames() {
 }
 
 function displayGifs(clickedId) {
-  if ($(".gif-area").length){
-      $(".gif-area").empty();
-      }  
-
-    for (let i = 0; i < existingEntries.storedGiphys[clickedId].length; i++) {
-
- 
-
-        let giphyLine = $("<div>")
-          .addClass("my-giphy-line")
-          .appendTo($(".gif-area"));
-      let gifColumnDiv = $("<div>")
-          .addClass("giphy-div giphy-divFav card card-select")
-          .appendTo(giphyLine);
-          $("<img>")
-          .addClass("giphysfav")
-          .attr("src", existingEntries.storedGiphys[clickedId][i])
-          .appendTo(gifColumnDiv);
-          $("<label>").text("").addClass("giphyLabel").appendTo(giphyLine)
-    
-          $("<input>").attr("type","text").addClass("embeding").val('<img src="' + existingEntries.storedGiphys[clickedId][i]+ '" width=”125″ height=”12″ />' ).appendTo(giphyLine)
-
+  trigger = true;
+  if ($(".gif-area").length) {
+    $(".gif-area").empty();
+  }
+  for (let i = 0; i < existingEntries.storedGiphys[clickedId].length; i++) {
+    let gifColumnDiv = $("<div>")
+      .addClass("column gif-div card")
+      .appendTo($(".gif-area"));
+    $("<img>")
+      .addClass("thumbnail")
+      .attr("src", existingEntries.storedGiphys[clickedId][i])
+      .appendTo(gifColumnDiv);
   }
 }
-
-function addListeneronGiphys() {
-$(".giphy-div").on("click", function(e){
-e.preventDefault();
-$(this).siblings("label").text("Link has Been Copied to Clipboard").show();
-let copyLink = $(this).children("img").attr("src")
-    const el = document.createElement('textarea');
-    el.value = copyLink;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    $(this).siblings("label").text("Link has Been Copied to Clipboard").fadeOut(3000);
-
-})
-
-
-
-}
-
 displaySearchNames()
 
-// event listener for list item clicks
-let listClickEl = $(".search-name");  
-listClickEl.on("click", function(event){
+let listClickEl = $(".search-name");
+listClickEl.on("click", function (event) {
   event.preventDefault();
-  // let clickedName = $(this).text();
+  let clickedName = $(this).text();
   let clickedId = $(this).attr('id');
   displayGifs(clickedId);
-  $(this).attr("backgroundColor","red")
   $(".search-name")
-      .attr("backgroundColor", "white")
-      .attr("color", "darkgray");
+    .css("backgroundColor", "white")
+    .css("color", "darkgray");
   $(event.target)
-    .attr("backgroundColor", "red")
-    .attr("color", "white");
-    addListeneronGiphys()
+    .css("backgroundColor", "red")
+    .css("color", "white");
 })
+
+
+let gifClickEl = $(".gif-area");
+gifClickEl.on("click", (function (event) {
+  document.getElementById("URLinput").style.display = "initial";
+  document.getElementById("copyUrlButton").style.display = "initial";
+  $("#URLinput")
+    .val("<img src='" + event.target.src + "'>");
+}))
+
+let copyUrlButton = $("#copyUrlButton")
+copyUrlButton.on("click", (function copyToClipboard() {
+  var gifURL = document.getElementById("URLinput");
+  gifURL.select();
+  gifURL.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Copied the URL: " + gifURL.value);
+}))
+
+
+
+
