36cad0f87b1cd6ede55353a51a84b30954068716
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-05-08 22:09:39 -0700

    my changes

--- assets/js/favoritesscript.js
+++ assets/js/favoritesscript.js
@@ -22,19 +22,47 @@ function displaySearchNames() {
 
 function displayGifs(clickedId) {
   if ($(".gif-area").length){
-    $(".gif-area").empty();
-    }  
+      $(".gif-area").empty();
+      }  
 
-  for (let i = 0; i < existingEntries.storedGiphys[clickedId].length; i++) {
-   let gifColumnDiv = $("<div>")
-          .addClass("giphy-div card card-select")
+    for (let i = 0; i < existingEntries.storedGiphys[clickedId].length; i++) {
+
+ 
+
+        let giphyLine = $("<div>")
+          .addClass("my-giphy-line")
           .appendTo($(".gif-area"));
-      $("<img>")
-          .addClass("giphys")
-          .attr("src", gifUrl)
+      let gifColumnDiv = $("<div>")
+          .addClass("giphy-div giphy-divFav card card-select")
+          .appendTo(giphyLine);
+          $("<img>")
+          .addClass("giphysfav")
+          .attr("src", existingEntries.storedGiphys[clickedId][i])
           .appendTo(gifColumnDiv);
+          $("<label>").text("").addClass("giphyLabel").appendTo(giphyLine)
+    
+          $("<input>").attr("type","text").addClass("embeding").val('<img src="' + existingEntries.storedGiphys[clickedId][i]+ '" width=”125″ height=”12″ />' ).appendTo(giphyLine)
 
   }
+}
+
+function addListeneronGiphys() {
+$(".giphy-div").on("click", function(e){
+e.preventDefault();
+$(this).siblings("label").text("Link has Been Copied to Clipboard").show();
+let copyLink = $(this).children("img").attr("src")
+    const el = document.createElement('textarea');
+    el.value = copyLink;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    $(this).siblings("label").text("Link has Been Copied to Clipboard").fadeOut(3000);
+
+})
+
+
+
 }
 
 displaySearchNames()
@@ -43,13 +71,15 @@ displaySearchNames()
 let listClickEl = $(".search-name");  
 listClickEl.on("click", function(event){
   event.preventDefault();
-  let clickedName = $(this).text();
+  // let clickedName = $(this).text();
   let clickedId = $(this).attr('id');
   displayGifs(clickedId);
+  $(this).attr("backgroundColor","red")
   $(".search-name")
-      .css("backgroundColor", "white")
-      .css("color", "darkgray");
+      .attr("backgroundColor", "white")
+      .attr("color", "darkgray");
   $(event.target)
-    .css("backgroundColor", "red")
-    .css("color", "white");
+    .attr("backgroundColor", "red")
+    .attr("color", "white");
+    addListeneronGiphys()
 })
